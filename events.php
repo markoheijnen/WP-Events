85ddb871eb937cf169f4eb0cc46b1f936e6833be
Marko Heijnen <info@markoheijnen.com>
2016-01-23 23:18:11 +0100

    Added shortcode for listing the events

--- events.php
+++ events.php
@@ -18,6 +18,77 @@ class Markoheijnen_Events {
 
 	public function __construct() {
 		new Markoheijnen_Events_CPT;
+
+		add_shortcode( 'events_list', array( $this, 'shortcode_list' ) );
+	}
+
+	public function shortcode_list( $atts ) {
+		$atts = shortcode_atts( array(
+			'types' => false,
+		), $atts, 'events_list' );
+
+		$args = array(
+			'post_type'      => 'event',
+			'posts_per_page' => -1
+		);
+
+		if ( $atts['types'] ) {
+			$args['tax_query'] = array(
+				array(
+					'taxonomy' => 'event-type',
+					'field'    => 'slug',
+					'terms'    => explode(',', $atts['types'] )
+				)
+			);
+		}
+
+		$events = get_posts( $args );
+
+		// Return when there are no events
+		if ( ! $events ) {
+			return '';
+		}
+
+		// Setting variables
+		$current_year = false;
+
+		$html = '<div class="row">';
+
+		foreach ( $events as $event ) {
+			$event_year = substr( $event->post_date, 0, 4 );
+
+			if ( $current_year != $event_year ) {
+				if ( $current_year ) {
+					$html .= '</ul>';
+					$html .= '</div>';
+				}
+
+				$html .= '<div class="col-sm-4">';
+				$html .= '<h2>' . $event_year . '</h2>';
+				$html .= '<ul>';
+			}
+			
+
+			$types = wp_get_post_terms( $event->ID, 'event-type', array( 'fields' => 'names' ) );
+
+			$html .= '<li>';
+			if ( in_array( 'speaker', $types ) ) {
+				$html .= '<strong>' . $event->post_title . '</strong>';
+			}
+			else {
+				$html .= $event->post_title;
+			}
+			$html .= '</li>';
+
+			// Set variables
+			$current_year = $event_year;
+		}
+
+		$html .= '</ul>';
+		$html .= '</div>';
+		$html .= '</div>';
+
+		return $html;
 	}
 
 }
